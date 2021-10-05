e33ac7f92d24dbebd9f0007935a2884d6204aeaf
Neelesh Chandwani <neeleshchandwani@gmail.com>
2021-10-05 18:24:21 +0530

    Form handling: controlled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,26 +4,38 @@ import './LoginSignup.css';
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.isEmail = React.createRef();
-        this.isPassword = React.createRef();
-        //Uncontrolled Components
+        this.state = {
+            email: '',
+            password: ''
+        }
     }
     handleLoginForm = (e) => {
         e.preventDefault();
-        console.log('this.isEmail: ', this.isEmail.current.value);
-        console.log('this.isPassword: ', this.isPassword.current.value);
+        console.log(this.state);
     }
+
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
     render() {
         return (
             <form className="login-form">
                 <h1 className='login-signup-header'>Login to Codeial</h1>
                 <div className="field">
                     <label htmlFor="login-email">Email</label>
-                    <input type="email" name="email" id="login-email" ref={this.isEmail} />
+                    <input type="email" name="email" id="login-email" onChange={this.handleEmailChange} value={this.state.email} />
                 </div>
                 <div className="field">
                     <label htmlFor="login-password">Password</label>
-                    <input type="password" name="password" id="login-password" ref={this.isPassword} />
+                    <input type="password" name="password" id="login-password" onChange={this.handlePasswordChange} value={this.state.password} />
                 </div>
                 <div className="field">
                     <button onClick={this.handleLoginForm}>Login</button>
